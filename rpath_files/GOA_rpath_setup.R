3302670d840cd4ea79be7b26f78939c24445dbcd
kerim aydin <kerim@uw>
2024-12-11 15:25:14 -0800

    fixed names in EwE files

--- rpath_files/GOA_rpath_setup.R
+++ rpath_files/GOA_rpath_setup.R
@@ -23,7 +23,7 @@ source("rpath_files/xml_convert.r")
 # WGOA creation ###############
 # Create unbalanced rpath object, don't worry about table alignment warnings
 # if the tables aren't ones used in basic path or stanzas.
-  unbal <- import.eiixml(eiifile = "rpath_files/WGOA_11Dec2024.eiixml")
+  unbal <- import.eiixml(eiifile = "rpath_files/WGOA_11Dec2024_renamed.eiixml")
 
 # WGOA hand-fixes from entry issues in the EwE model.  These should be
 # fixed eventually in the EwE files.  
@@ -45,11 +45,11 @@ source("rpath_files/xml_convert.r")
 # then balance the model, copying over to western version.
   w.unbal <- rpath.stanzas(unbal)
   check.rpath.params(w.unbal)
-  w.bal   <- rpath(unbal)
+  w.bal   <- rpath(w.unbal)
 
 # EGOA creation ###############  
   # Create unbalanced rpath object
-  unbal <- import.eiixml(eiifile = "rpath_files/EGOA-multi-stanza_2024_12_10.eiixml",export=T)
+  unbal <- import.eiixml(eiifile = "rpath_files/EGOA-multi-stanza_2024_12_10_renamed.eiixml",export=T)
   
   # WGOA hand-fixes from entry issues in the EwE model.  These should be
   # fixed eventually in the EwE files. 
@@ -68,25 +68,25 @@ source("rpath_files/xml_convert.r")
   # then balance the model, copying over to western version.
   e.unbal <- rpath.stanzas(unbal)
   check.rpath.params(e.unbal)
-  e.bal   <- rpath(unbal)
+  e.bal   <- rpath(e.unbal)
   
 #############################################################  
-# SCRATCH STUFF not 
+# SCRATCH STUFF not needed for now 
   
-impdiet <- ordgroups$ImpVar; names(impdiet)<-rownames(ordgroups)    
-predlist <- names(unbal$diet)[2:ncol(unbal$diet)]
-for(p in predlist){
-  unbal$diet[Group=="Import", p] <- impdiet[p]
-}
-  
-# To check Biomass, EEs etc.
-View(data.frame(bal$Biomass, bal$PB, bal$QB, bal$EE))
-
-# Testing rsim
-scene0 <- rsim.scenario(bal, unbal, years=1990:2089)
-run0   <- rsim.run(scene0, method="RK4", years = 1990:2089)
-rsim.plot(run0)
-
-scene1 <- adjust.fishing(scene0, "ForcedFRate", "sablefish_adult", sim.year=1995:2000, value=0.2)
-run1   <- rsim.run(scene1, method="RK4", years = 1990:2089)
-rsim.plot(run1)
+# impdiet <- ordgroups$ImpVar; names(impdiet)<-rownames(ordgroups)    
+# predlist <- names(unbal$diet)[2:ncol(unbal$diet)]
+# for(p in predlist){
+#   unbal$diet[Group=="Import", p] <- impdiet[p]
+# }
+#   
+# # To check Biomass, EEs etc.
+# View(data.frame(bal$Biomass, bal$PB, bal$QB, bal$EE))
+# 
+# # Testing rsim
+# scene0 <- rsim.scenario(bal, unbal, years=1990:2089)
+# run0   <- rsim.run(scene0, method="RK4", years = 1990:2089)
+# rsim.plot(run0)
+# 
+# scene1 <- adjust.fishing(scene0, "ForcedFRate", "sablefish_adult", sim.year=1995:2000, value=0.2)
+# run1   <- rsim.run(scene1, method="RK4", years = 1990:2089)
+# rsim.plot(run1)
--- rpath_files/WGOA_EGOA_comp.R
+++ rpath_files/WGOA_EGOA_comp.R
@@ -1,44 +1,12 @@
 library(Rpath)
 
-# Script assumes wd is WGOA-Ecopath_Rpath main repo (root of repo)
+# load models into w.unbal, w.bal for west, e.unbal and e.bal for east.
+  source("rpath_files/GOA_rpath_setup.R")
 
-# Read in eiiXML file at the location given by eiipath (currently as a source
-# file for debugging, will later be a function incorporated into rpath).
-# xml_convert.r should contain generic read-ins only.
-#eiifile <- "WGOA_EwE_Files/WGOA_Nov2024.eiixml"
-source("rpath_files/xml_convert.r")
+# Groups in the west not the east
+  w.unbal$model$Group[which(!(w.unbal$model$Group %in% e.unbal$model$Group))]
 
-
-# WGOA ############################
-  w.unbal <- import.eiixml("WGOA_EwE_Files/WGOA_Nov2024.eiixml")
-  # TODO WGOA note Lingcod name issue
-  # TODO WGOA names in general
-
-# WGOA hand-fixes from entry issues in the EwE model.  These should be
-# fixed eventually in the EwE files.  Odd lookup here (data.table quirks). 
-  w.unbal$model[Type==2, "DetInput"] <- 0
-  det_names <- w.unbal$model[Type==2]$Group
-  w.unbal$model[Group%in%det_names, det_names] <- 0
-  # TODO RPATH:  COME BACK TO DETRITAL FLOWS AND WARNINGS (what should they be??)
-  # TODO RPATH:  ALSO, add warning for Biomass versus PB for detritus?
-
-# Remove Biomass and add PB to detritus to estimate flows
-  w.unbal$model[Group%in%det_names, "Biomass"] <- NA
-  w.unbal$model[Group%in%det_names, "PB"]      <- 1.0
-
-# IGNORE DETRITAL FLOW WARNINGS IN check.rpath.params FOR NOW  
-  w.unbal <- rpath.stanzas(w.unbal)
-  check.rpath.params(w.unbal)
-  w.bal <- rpath(w.unbal)
-
+# Groups in the east not the west 
+  e.unbal$model$Group[which(!(e.unbal$model$Group %in% w.unbal$model$Group))]
   
-# EGOA ##################  
-  e.unbal <- import.eiixml("WGOA_EwE_Files/EGOA-multi-stanza.eiixml")
-  det_names <- e.unbal$model[Type==2]$Group
-  e.unbal$model[Group%in%det_names, det_names] <- 0  
   
-  e.unbal
-  
-  e.unbal <- rpath.stanzas(e.unbal)
-  check.rpath.params(e.unbal)
-  e.bal <- rpath(e.unbal)
