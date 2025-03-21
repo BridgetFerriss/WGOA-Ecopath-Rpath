cc01f9b219bf4647b336d2122dcb598ce760753d
Andy Whitehouse <andy.whitehouse@noaa.gov>
2025-03-21 14:46:17 -0700

    re-commit v2

--- code/FourSystems_comp.R
+++ code/FourSystems_comp.R
@@ -67,52 +67,18 @@ library(dplyr)
   
   
   
-flowmat <- function(bal, timesteps=365){
-
-  main_flows <- matrix(0, nrow=bal$NUM_GROUPS+1, ncol=bal$NUM_GROUPS+1)
-  rownames(main_flows) <- c(bal$Group, "Import")
-  colnames(main_flows) <- c(bal$Group, "Import")
-
-# diet flows (QB scaled, no respiration/egestion)  
-  sz <- dim(bal$DC)
-  dmat <- t(matrix(bal$DC, nrow=sz[1], ncol=sz[2]))
-  pmat <- t(bal$QB[1:bal$NUM_LIVING] * bal$Biomass[1:bal$NUM_LIVING] * dmat / timesteps)
+fup<-function(){source("code/FourSystems_functions.R")}
   
-  main_flows[1:(bal$NUM_LIVING+bal$NUM_DEAD),1:bal$NUM_LIVING] <- 
-    main_flows[1:(bal$NUM_LIVING+bal$NUM_DEAD),1:bal$NUM_LIVING] +
-    pmat[1:(bal$NUM_LIVING+bal$NUM_DEAD),1:bal$NUM_LIVING]
-  main_flows["Import",1:bal$NUM_LIVING] <- 
-    main_flows["Import",1:bal$NUM_LIVING] + pmat[nrow(pmat),]
-
-# Biomass flows   
-  bmat <- diag(c(bal$Biomass[1:(bal$NUM_LIVING+bal$NUM_DEAD)],rep(1.0,bal$NUM_GEARS), 0.0))
-  main_flows <- main_flows + bmat
-  
-# Fishing flows  
-  fmat <- (bal$Landings + bal$Discards)/timesteps
-  main_flows[1:bal$NUM_GROUPS, (bal$NUM_GROUPS-bal$NUM_GEARS+1):bal$NUM_GROUPS] <- 
-    main_flows[1:bal$NUM_GROUPS,(bal$NUM_GROUPS-bal$NUM_GEARS+1):bal$NUM_GROUPS] +
-    fmat
-
-# Mzero detritial flows
-  
-  
-# Convert flow vector into transition state (proportion by row)  
-  transition <- main_flows/rowSums(main_flows)[row(main_flows)]
-  transition[is.nan(transition) | is.na(transition)] <- 0
-  
-# Make state vector  
-  state <- matrix(rep(0, bal$NUM_GROUPS+1),nrow=1); colnames(state)<-c(bal$Group,"Import")
-  
-  return(list(state=state,transition=transition))
-}  
 
 
 transvec <- rep(0,w.bal$NUM_GROUPS+1); names(transvec)<-c(w.bal$Group,"Import")
 transvec["small_phytoplankton"] <- 1
 
+det_names <- w.unbal$model[Type==2]$Group
+w.bal$PB*w.bal$Biomass*(1.0-w.bal$EE) * w.bal$DetFate
 
-steps <- 365
+
+steps <- 36500
 
 w.markov <- flowmat(w.bal)
 w.markov$state[,"small_phytoplankton"] <- 1
@@ -123,7 +89,8 @@ for (i in 1:steps){
    w.markov$state <- w.markov$state %*% w.markov$transition
 }
   
-    
+ 
+   
 # Checking group names #########################################################
 # Groups in the west not the east
   w.unbal$model$Group[which(!(w.unbal$model$Group %in% e.unbal$model$Group))]
