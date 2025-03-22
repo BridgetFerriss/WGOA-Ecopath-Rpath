82fc9e650d2520da9e200faa2ebbfb78306b2d0e
Andy Whitehouse <andy.whitehouse@noaa.gov>
2025-03-22 14:47:55 -0700

    Added an example loop to run many simulations and an example of how to access sim results.

--- code/simulations/goa_sims.r
+++ code/simulations/goa_sims.r
@@ -2,7 +2,27 @@ library('viridis')
 library('here')
 source("code/simulations/goaclim_sim_v4.r")
 source("code/bioenergetic_projections.r")
-# arguments: scene, ssp, hcr, cons, resp, buf
-test_run_126 <- clim_sim_hcr_obs_err(scene, 126, 1, TRUE, TRUE, TRUE)
-test_run_245 <- clim_sim_hcr_obs_err(scene, 245, 1, TRUE, TRUE, TRUE)
-test_run_585 <- clim_sim_hcr_obs_err(scene, 585, 1, TRUE, TRUE, TRUE)
+num_runs <- 100
+sim_126_f1 <- as.list(rep(NA,num_runs))
+sim_245_f1 <- as.list(rep(NA,num_runs))
+sim_585_f1 <- as.list(rep(NA,num_runs))
+set.seed(13)
+
+# arguments: (scenario, ssp, hcr, cons, resp, buf)
+# test_run_126 <- clim_sim_hcr_obs_err(scene, 126, 1, TRUE, TRUE, TRUE)
+# test_run_245 <- clim_sim_hcr_obs_err(scene, 245, 1, TRUE, TRUE, TRUE)
+# test_run_585 <- clim_sim_hcr_obs_err(scene, 585, 1, TRUE, TRUE, TRUE)
+
+ptm <- proc.time()
+for(i in 1:num_runs){
+  print(paste0("num_run_",i))
+  sim_126_f1[[i]] <- clim_sim_hcr_obs_err(scene, 126, 1, TRUE, TRUE, TRUE)
+  print(paste0("num_run_",i))
+  sim_245_f1[[i]] <- clim_sim_hcr_obs_err(scene, 245, 1, TRUE, TRUE, TRUE)
+  print(paste0("num_run_",i))
+  sim_585_f1[[i]] <- clim_sim_hcr_obs_err(scene, 585, 1, TRUE, TRUE, TRUE)
+}
+proc.time() - ptm
+
+# to access sim outputs, for example
+sim_126_f1[[1]]$annual_Catch[,"walleye_pollock_adult"]
