cc0ab0c4d4876f11217ff4ff9cd4d9a63e3d0225
Bia Dias <bia.dias@noaa.gov>
2025-04-30 22:56:29 -0600

    Editing the code to account for the stratum bins and the juvenile and adult biomasses.

--- code/race_biomass_time_series.R
+++ code/race_biomass_time_series.R
@@ -32,8 +32,8 @@ source("code/REEM_fooddata_functions.R")
 REEM.loadclean.RACE.googledrive("https://drive.google.com/drive/folders/1nIXG6ydT52bcy-UoEgFSjKYWUXvtx-JA")
 
 # Load strata table (linking models to strata)
-REEM.loadclean.strata(strata_lookup_file    = "lookups/combined_BTS_strata.csv",
-                      stratum_bin_column    = "strat_groups")
+REEM.loadclean.strata.by.stratum(strata_lookup_file    = "lookups/combined_BTS_strata.csv",
+                      stratum_bin_column    = "stratum") # here we are using the individual stratum instead stratum groups
 
 
 # Load lookup file for mapping RACE codes to biomass. Later need to specify
@@ -68,17 +68,32 @@ race_lookup_col <- c("EBS" = "ebs_ecopath",
 this.model  <- "WGOA"
 race_lookup      <- race_lookup_base %>% mutate(race_group  = .data[["final_wgoa"]])
 q_table          <- read.clean.csv("lookups/GroupQ_2021_GOA.csv")
-domains_included <-  c(
-  "Chirikof_shelf",
-  "Chirikof_gully",
-  "Chirikof_slope",
-  "Kodiak_shelf",
-  "Kodiak_gully",
-  "Kodiak_slope",
-  "Shumagin_shelf",
-  "Shumagin_gully",
-  "Shumagin_slope"
+#domains_included <-  c(
+#  "Chirikof_shelf",
+#  "Chirikof_gully",
+#  "Chirikof_slope",
+#  "Kodiak_shelf",
+#  "Kodiak_gully",
+#  "Kodiak_slope",
+#  "Shumagin_shelf",
+#  "Shumagin_gully",
+#  "Shumagin_slope"
+#)
+
+#Explanation of the domains included: Here we are using the stratum bins instead of the full domain, since with have higher confidence in the data for doing so.
+#However, for temperature guilds and diet we are going with the main domains. 
+
+domains_included <-  c("20", "21", "22", "121", "122",    #  "Chirikof_shelf" 
+                       "120", "220",                      #  "Chirikof_gully"
+                       "221", "320", "420", "520",        #  "Chirikof_slope"
+                       "130", "230", "232",               #  "Kodiak_shelf"
+                       "30", "31","32", "33", "35","131", "132", "133", "134", #  "Kodiak_gully"
+                       "231", "330", "430", "530",        #  "Kodiak_slope"
+                       "10", "11", "12", "13","111",      #  "Shumagin_shelf"
+                       "110", "112",                      #  "Shumagin_gully"
+                       "210", "310", "410", "510"         #  "Shumagin_slope"
 )
+
 
 tot_model_area <- sum(strat_areas$area[strat_areas$model == this.model &
                                          strat_areas$stratum_bin %in% domains_included])
@@ -112,7 +127,8 @@ domain_sum <- haul_sum %>%
     var_wtcpue    = tot_wtcpue2 / stations - mean_wtcpue * mean_wtcpue,
     bio_t_km2     = mean_wtcpue / 1000,
     #WHY the cpue to mt/km2 conversion is 1:1000? ####
-    #units are kg/hectare and are transformed by the GAP_get_cpue.R function. here we are doing the final transformation from kg to mt
+    #units are kg/hectare and are transformed by the GAP_get_cpue.R function. 
+    #Here we are doing the final transformation from kg to mt
     bio_tons      = bio_t_km2 * area,
     var_bio_t_km2 = var_wtcpue / (1000 * 1000),
     var_bio_tons  = var_wtcpue * (area / 1000) * (area / 1000)
@@ -146,6 +162,83 @@ model_sum <- domain_sum %>%
   fill(model_area, .direction = "down") %>%
   ungroup()
 
+
+
+predlist <- read.clean.csv("lookups/Alaska_Multistanza_GOA_vonb_2025_04_30_v2.csv")
+#race_lookup <- read.clean.csv("lookups/race_lookup_base_v2.csv") %>% 
+#  mutate(race_guild  = .data[["final_wgoa"]])
+
+preds      <- predlist %>% filter(model==this.model)
+pred_names <- unique(preds$predator)
+pred_params=list()
+for (p in pred_names){
+  pdat <- as.list(preds[preds$predator==p,])
+  pred_params[[p]] <- pdat
+  pred_params[[p]]$LCLASS <- sort(unique(c(0,pdat$juv_cm, pdat$adu_1, pdat$adu_2, pdat$adu_3,9999)))
+  pred_params[[p]]$jsize  <- paste("[",pred_params[[p]]$LCLASS[1],",",pred_params[[p]]$LCLASS[2],")",sep='')
+  pred_params[[p]]$lw_b   <- pdat$b_l_mm_g
+  pred_params[[p]]$lw_a   <- pdat$a_l_mm_g*(10^pdat$b_l_mm_g)  
+  pred_params[[p]]$bioen  <- list(CA=pdat$ca, CB=pdat$cb, C_TM=pdat$c_tm, C_T0=pdat$c_t0, C_Q=pdat$c_q)
+  pred_params[[p]]$vonb <- list(h= pdat$vb_k, Linf  = pdat$vb_linf_mm, t0= pdat$vb_t0, rec_len=pdat$von_b_rec_len_cm)
+}
+
+
+
+
+#Getting biomass-at-length and converting to split pool biomass
+# Loop through each predator.  the juv_adu_lencons splits out
+# length samples based on the length class divisions in the
+# predator lookup, and converts to % by weight using the a and b LW
+# regression.  This % by weight is applied to the total biomass
+# for the predator calculated above, to calculate biomass in
+# each length class.  It is output into bio_combined as two biomass groupings
+# (juv and adu) where adu is the sum of all the adu size classes.
+
+
+bio_totals <- domain_sum %>%
+  group_by(year, model, race_group) %>%
+  summarize(bio_tons = sum(bio_tons),
+            bio_tkm2 = bio_tons/tot_model_area, .groups="keep")
+
+juv_combined <- NULL
+for (p in pred_names){
+  #p <- pred_names[1]
+  bio_pred <- bio_totals %>%
+    filter(race_group==p)
+  
+  juv_adu_lencons  <- get_stratum_length_cons(predator=p, model=this.model) %>%
+    group_by(year, model, species_name, stratum, lbin) %>%
+    summarize(strat_bio_sum = sum(tot_wlcpue_t_km2), .groups="keep") %>%
+    left_join(haul_stratum_summary(this.model),by=c("year","model","stratum")) %>%
+    mutate(bio_t_km2 = strat_bio_sum/stations,
+           bio_tons  = bio_t_km2 * area,
+           jcat      = ifelse(lbin==pred_params[[p]]$jsize,"juv","adu")) 
+  
+  juv_proportions <- juv_adu_lencons %>%
+    group_by(year,model,species_name,jcat) %>%
+    summarize(bio_tons = sum(bio_tons), .groups="keep") %>%
+    pivot_wider(names_from=jcat, values_from=bio_tons) %>%
+    mutate(juv_bio_prop = juv/(juv+adu))
+  
+  juv_combined <- rbind(juv_combined,juv_proportions)
+} # end of pred_names loop
+
+bio_with_juvs <- bio_totals %>%
+  left_join(juv_combined,by=c("year","model","race_group"="species_name")) %>%
+  select(-c(juv,adu)) %>%
+  mutate(juv_bio_prop  = replace_na(juv_bio_prop,0.0),
+         adu_bio_tkm2  = (1.0-juv_bio_prop) * bio_tkm2,
+         juv_bio_tkm2  = juv_bio_prop       * bio_tkm2)
+
+bio_combined <- NULL
+bio_combined <- rbind(bio_combined,bio_with_juvs)
+
+
+write.csv(bio_combined,
+          "WGOA_source_data/WGOA_groundfish_bio_juv_adu.csv",
+          row.names = F)
+
+#BIA YOU ARE HERE ####
 #wide format
 wgoa_race_b <- model_sum %>% 
   select(year,model,race_group,bio_tons_km2,cv_bio_tons_km2) %>%
